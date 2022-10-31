7dae3c775bc4be4ab6828e588094e1091ab9b8db
Timothy Karani <kartimothy@gmail.com>
2022-10-31 14:41:45 +0300

    Can send text/receive response

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MantineProvider, createEmotionCache } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const customCache = createEmotionCache({
+  key: "mantine",
+  prepend: false,
+});
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: "light",
+      }}
+      emotionCache={customCache}
+    >
+      <NotificationsProvider>
+        <Component {...pageProps} />
+      </NotificationsProvider>
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
